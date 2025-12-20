5b38a05b85203f50f912d07614d6050a9d79883a
linmjie <linmgjie@gmail.com>
2025-12-19 22:46:15 -0500

    debugger

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -125,10 +125,69 @@ require("lazy").setup(
                 })
               })
     end
-    }
+    },
+
+
+    --DEBUG TOOLS
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+            dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"}
+        },
+        config = function()
+              local dap = require("dap")
+              local dapui = require("dapui")
+
+              require("dapui").setup()
+              dap.listeners.before.attach.dapui_config = function()
+                  dapui.open()
+              end
+              dap.listeners.before.launch.dapui_config = function()
+                  dapui.open()
+              end
+              dap.listeners.before.event_terminated.dapui_config = function()
+                  dapui.close()
+              end
+              dap.listeners.before.event_exited.dapui_config = function()
+                  dapui.close()
+              end
+
+              dap.adapters.cppdbg = {
+                  id = 'cppdbg',
+                  type = 'executable',
+                  command = '/home/lin/.vscode/extensions/ms-vscode.cpptools-1.29.3-linux-x64/debugAdapters/bin/OpenDebugAD7',
+              }
+
+              dap.configurations.cpp = {
+              {
+                name = "Launch file",
+                type = "cppdbg",
+                request = "launch",
+                program = function()
+                  return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+                cwd = '${workspaceFolder}',
+                stopAtEntry = true,
+              },
+              {
+                name = 'Attach to gdbserver :1234',
+                type = 'cppdbg',
+                request = 'launch',
+                MIMode = 'gdb',
+                miDebuggerServerAddress = 'localhost:1234',
+                miDebuggerPath = '/usr/bin/gdb',
+                cwd = '${workspaceFolder}',
+                program = function()
+                  return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                end,
+              },
+            }
+        end
+    },
 
   },
-  -- Configure any other settings here. See the documentation for more details.
+
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
