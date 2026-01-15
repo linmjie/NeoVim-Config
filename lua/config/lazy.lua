e65394d17c77e67769ca7a1d8793a4e7afaf4913
linmjie <linmgjie@gmail.com>
2026-01-15 09:01:27 -0500

    i think snippets work with blink.cmp now

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -115,12 +115,12 @@ require("lazy").setup(
         local cmp = require("cmp")
         require("luasnip.loaders.from_vscode").lazy_load()
             cmp.setup({
-                snippet = {
-                  -- REQUIRED - you must specify a snippet engine
-                  expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                  end,
-                },
+                --snippet = {
+                --  -- REQUIRED - you must specify a snippet engine
+                --  expand = function(args)
+                --    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                --  end,
+                --},
                 window = {
                   --completion = cmp.config.window.bordered(),
                   documentation = cmp.config.window.bordered(),
@@ -186,6 +186,7 @@ require("lazy").setup(
           sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer' },
           },
+
 
           -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
           -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
